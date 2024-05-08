9f9231138d020f1838b286e47be31173a5607e8f
Aleksa Stefanovic <aleksa.stefanovic0223@gmail.com>
2024-05-08 14:15:25 +0200

    made sidenav responsive for laptops and pcs

--- layout.php
+++ layout.php
@@ -7,32 +7,51 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
+    <style>
+        @media (max-width: 1439px) {
+            .sidebar {
+                width: 80px; /* Adjust width as needed */
+            }
+            .nav-link-text {
+                display: none;
+            }
+            .nav-link-icon {
+                margin-right: 0;
+            }
+            .fas {
+                font-size: 20px;
+            }
+            .fa-school {
+                font-size: 40px;
+            }
+        }
+    </style>
 </head>
 <body>
     <div class="col-2 p-0">
         <div class="d-flex flex-column flex-shrink-0 p-3 sidebar" style="height: 100vh;">
             <a href="/upis_plus" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                <i class="fas fa-school me-2 text-white"></i>
-                <span class="fs-4 text-white">UpisPlus</span>
+                <i class="fas fa-school text-white nav-link-icon"></i>
+                <span class="fs-4 text-white nav-link-text">UpisPlus</span>
             </a>
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li>
                     <a href="/upis_plus/admin" class="nav-link link-light">
-                        <i class="fas fa-user-shield me-2"></i>
-                        Admin
+                        <i class="fas fa-user-shield nav-link-icon"></i>
+                        <span class="nav-link-text">Admin</span>
                     </a>
                 </li>
                 <li>
                     <a href="/upis_plus/statistika-upisa-ucenika" class="nav-link link-light">
-                        <i class="fas fa-chart-line me-2"></i>
-                        Statistika Upisa Učenika
+                        <i class="fas fa-chart-line nav-link-icon"></i>
+                        <span class="nav-link-text">Statistika Upisa Učenika</span>
                     </a>
                 </li>
                 <li>
                     <a href="/upis_plus/statistika-upisa-fakulteta" class="nav-link link-light">
-                        <i class="fas fa-university me-2"></i>
-                        Statistika Upisa Na Fakultete
+                        <i class="fas fa-university nav-link-icon"></i>
+                        <span class="nav-link-text">Statistika Upisa Na Fakultete</span>
                     </a>
                 </li>
             </ul>
