aeaf0b9b2f012660bf4fccc2a74ef25136725ee9
StefanovicAleksa <aleksa.stefanovic0223@gmail.com>
2024-05-23 23:44:50 +0200

    removed comments for sttatistika upisa na fakultete/functions.php

--- statistika-upisa-fakulteta/functions.php
+++ statistika-upisa-fakulteta/functions.php
@@ -1,18 +1,14 @@
 <?php
 include '../config.php';
 
-// Function to display data from the view view_lista_upisa_po_fakultetu
 function displayViewListaUpisaPoFakultetu() {
     include '../config.php';
-    // Query to fetch data from the view
+
     $sql = "SELECT * FROM view_lista_upisa_po_fakultetu";
 
-    // Execute the query
     $result = $conn->query($sql);
 
-    // Check if there are rows returned
     if ($result->num_rows > 0) {
-        // Loop through each row and display the data
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row['ID Fakulteta'] . "</td>";
@@ -28,18 +24,14 @@ function displayViewListaUpisaPoFakultetu() {
     }
 }
 
-// Function to display the result of the stored procedure "Popularnost Smerova"
 function displayPopularnostSmerova() {
     include '../config.php';
-    // Query to call the stored procedure
+
     $sql = "CALL PopularnostSmerova()";
 
-    // Execute the query
     $result = $conn->query($sql);
 
-    // Check if there are rows returned
     if ($result->num_rows > 0) {
-        // Loop through each row and display the data
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row['Fakultet'] . "</td>";
@@ -52,18 +44,15 @@ function displayPopularnostSmerova() {
     }
 }
 
-// Function to display the result of the stored procedure "Prosecan Broj Upisanih Studenata Po Godini Za Svaki Fakultet"
+
 function displayProsecanBrojUpisanihStudenataPoGodiniZaSvakiFakultet() {
     include '../config.php';
-    // Query to call the stored procedure
+
     $sql = "CALL ProsecanBrojUpisanihStudenataPoGodiniZaSvakiFakultet()";
 
-    // Execute the query
     $result = $conn->query($sql);
 
-    // Check if there are rows returned
     if ($result->num_rows > 0) {
-        // Loop through each row and display the data
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row['Naziv_fakulteta'] . "</td>";
@@ -75,18 +64,14 @@ function displayProsecanBrojUpisanihStudenataPoGodiniZaSvakiFakultet() {
     }
 }
 
-// Function to display the result of the stored procedure "Fakulteti Sa Najvećim Brojem Upisanih Studenata"
 function displayFakultetiSaNajvecimBrojemUpisanihStudenata() {
     include '../config.php';
-    // Query to call the stored procedure
+
     $sql = "CALL FakultetiSaNajvecimBrojemUpisanihStudenata()";
 
-    // Execute the query
     $result = $conn->query($sql);
 
-    // Check if there are rows returned
     if ($result->num_rows > 0) {
-        // Loop through each row and display the data
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row['Naziv_fakulteta'] . "</td>";
@@ -98,18 +83,14 @@ function displayFakultetiSaNajvecimBrojemUpisanihStudenata() {
     }
 }
 
-// Function to display the result of the stored procedure "Broj Upisanih Studenata Na Svakom Fakultetu Po Polu"
 function displayBrojUpisanihStudenataNaSvakomFakultetuPoPolu() {
     include '../config.php';
-    // Query to call the stored procedure
+
     $sql = "CALL BrojUpisanihStudenataNaSvakomFakultetuPoPolu()";
 
-    // Execute the query
     $result = $conn->query($sql);
 
-    // Check if there are rows returned
     if ($result->num_rows > 0) {
-        // Loop through each row and display the data
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row['Naziv_fakulteta'] . "</td>";
